488a7fec7976d48ddd7cb4d695bed168b1d7e327
Achraf Kassioui <achrafkassioui@gmail.com>
2025-11-26 21:53:57 +0100

    Initial Commit

---
+++ SKRenderer-Demo/SKRenderer_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SKRenderer_DemoApp.swift
+//  SKRenderer-Demo
+//
+//  Created by Achraf Kassioui on 26/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SKRenderer_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
